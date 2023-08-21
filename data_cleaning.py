abd37d0c056f67c50c6dab938760481745a7c04f
Fakhrul Azran <fakhrulazran@gmail.com>
2023-08-21 12:19:18 +0800

    update path

--- data_cleaning.py
+++ data_cleaning.py
@@ -23,7 +23,7 @@ def is_good_image(filename):
 
 def main():
 
-    source = 'C:/GitHub/dds_drone_ws/workspace/training_dds/images'
+    source = '../images'
     print('Start cleaning corrupted images')
     images = [f for f in os.listdir(source)
               if re.search(r'([a-zA-Z0-9\s_\\.\-\(\):])+(.jpg|.jpeg|.png)$', f)]
